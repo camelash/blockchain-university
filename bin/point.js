fc93e496b480ed64a04a0ec6dc715f066ca99c07
Ryan X. Charles <ryanxcharles@gmail.com>
2016-03-24 17:13:04 -0700

    point example

---
+++ bin/point.js
@@ -0,0 +1,18 @@
+'use strict'
+let BN = require('fullnode/lib/bn')
+let Point = require('fullnode/lib/point')
+
+let x = BN(5)
+let point = Point.fromX(true, x)
+console.log(point.getX().toString(), point.getY().toString())
+
+let x2 = BN(6)
+let point2 = Point.fromX(true, x2)
+console.log(point2.getX().toString(), point2.getY().toString())
+
+let point3 = point.add(point2)
+console.log(point3.getX().toString(), point3.getY().toString())
+
+let x4 = BN(6)
+let point4 = point3.mul(x4)
+console.log(point4.getX().toString(), point4.getY().toString())
